a20f9fbc6531b87c32569ab1ea8fbd69ae2b1719
fadywadea <fwadea3@gmail.com>
2023-07-26 19:44:21 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 var arrayOfQuotes = [
   {
     "quote": "Be yourself; everyone else is already taken.",
-    "author": "-- Oscar Wilde"
+    "author": "Oscar Wilde"
   },
   {
     "quote": "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
-    "author": "-- Marilyn Monroe"
+    "author": "Marilyn Monroe"
   },
   {
     "quote": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-    "author": "-- Albert Einstein"
+    "author": "Albert Einstein"
   },
   {
     "quote": "So many books, so little time.",
-    "author": "-- Frank Zappa"
+    "author": "Frank Zappa"
   },
   {
     "quote": "A room without books is like a body without a soul.",
-    "author": "-- Marcus Tullius Cicero"
+    "author": "Marcus Tullius Cicero"
   },
   {
     "quote": "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
-    "author": "-- Bernard M. Baruch"
+    "author": "Bernard M. Baruch"
   },
   {
     "quote": "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
-    "author": "-- Dr. Seuss"
+    "author": "Dr. Seuss"
   }
 ];
 
@@ -38,6 +38,6 @@ function randomQuote() {
     randomQuote();
   } else {
     document.getElementById("quoteOutput").innerText = newQuote;
-    document.getElementById("authorOutput").innerText = `${arrayOfQuotes[quoteIndex].author}`;
+    document.getElementById("authorOutput").innerText = `-- ${arrayOfQuotes[quoteIndex].author}`;
   }
 };
